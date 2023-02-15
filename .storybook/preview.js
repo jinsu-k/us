1d346143fd6bfcc6b87a9c448bf4ecdf7a917d8d
Jinsu <wlstn8738@naver.com>
2023-02-15 13:23:18 +0900

    [Chore] #1: Storybook 초기값 설정 및 사용하지 않는 파일 제거

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,26 @@
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+
+import { usTheme } from 'Styles/Theme/theme';
+import { GlobalStyle } from 'Styles/Global/GlobalStyle';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story, context) => (
+    <RecoilRoot>
+      <ThemeProvider theme={usTheme}>
+        <GlobalStyle />
+        <Story {...context} />
+      </ThemeProvider>
+    </RecoilRoot>
+  ),
+];
