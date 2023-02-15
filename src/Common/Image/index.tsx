55672b97360ed6552da4eddba550614c39919c17
Jinsu <wlstn8738@naver.com>
2023-02-15 14:42:24 +0900

    [Feat] #1: 공통 Image 컴포넌트 추가

---
+++ src/Common/Image/index.tsx
@@ -0,0 +1,26 @@
+type Props = {
+  imgUrl: string;
+  w?: number | string;
+  h?: number | string;
+  alt?: string;
+};
+
+/**
+ * 공통 이미지 View 컴포넌트
+ * @param imgUrl - 이미지 URL
+ * @param w - 커스텀 이미지 width | default: 150 px
+ * @param h - 커스텀 이미지 height | default: 150 px
+ * @param alt - alt 텍스트
+ */
+export default function Image({ imgUrl, w = 150, h = 150, alt = '' }: Props) {
+  return (
+    <span>
+      <img
+        src={imgUrl}
+        width={typeof w === 'number' ? `${w}px` : w}
+        height={typeof h === 'number' ? `${h}px` : h}
+        alt={alt}
+      />
+    </span>
+  );
+}
