5de8a5a007dafadcf742383c3d902d6949a8b249
Jinsu <wlstn8738@naver.com>
2023-02-23 18:04:55 +0900

    [Feat] #4: 로그인 View 추가

--- src/Pages/SignIn/index.tsx
+++ src/Pages/SignIn/index.tsx
@@ -1,6 +1,62 @@
+import Layout from 'Common/Layout/Layout';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
+import { currentUserState } from 'Recoils';
+import {
+  SiginInButton,
+  SiginInInput,
+  SiginInInputBlock,
+  SiginInTitle,
+  SignInContainer,
+  SignInOptionBlock,
+  SignInWrapper,
+  SingInOptionText,
+} from './SignIn.style';
+
 /**
  * 로그인 페이지
  */
 export default function SignIn() {
-  return <div>Sign In</div>;
+  const navigator = useNavigate();
+  const setCurrentUser = useSetRecoilState(currentUserState);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // 임시 로그인 기능
+  const signIn = () => {
+    setCurrentUser(true);
+    navigator('/');
+  };
+
+  return (
+    <Layout>
+      <SignInContainer>
+        <SignInWrapper>
+          <SiginInTitle>로그인</SiginInTitle>
+          <SiginInInputBlock>
+            <SiginInInput
+              placeholder={'이메일'}
+              type={'EMAIL'}
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+            <SiginInInput
+              placeholder={'비밀번호'}
+              type={'PASSWORD'}
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </SiginInInputBlock>
+          <SiginInButton onClick={signIn}>로그인</SiginInButton>
+          <SignInOptionBlock>
+            <SingInOptionText>회원가입</SingInOptionText>
+            <SingInOptionText>아이디찾기</SingInOptionText>
+            <SingInOptionText>비밀번호찾기</SingInOptionText>
+          </SignInOptionBlock>
+        </SignInWrapper>
+      </SignInContainer>
+    </Layout>
+  );
 }
